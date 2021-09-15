3fca144b787ff32323f91b434c277f525b8a46fe
krishna712001 <63452545+krishna712001@users.noreply.github.com>
2021-09-15 23:49:32 +0800

    Edge detector working

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import cvzone
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 img = cv2.imread("Images/homer.jpeg")
 savingPath = "/Users/krishnakothandaraman/PycharmProjects/backgroundRemovalFromImage/processedImages"
@@ -9,6 +11,33 @@ segmenter = SelfiSegmentation()
 
 imgOut = segmenter.removeBG(img, (255, 255, 255), threshold=0.5)
 
-cv2.imwrite(os.path.join(savingPath,'processed.jpg'), imgOut)
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+height, width = gray_img.shape
 
-print("done")
+# white_padding = np.zeros((50, width, 3))
+# white_padding[:, :] = [255, 255, 255]
+# rgb_img = np.row_stack((white_padding, img))
+
+gray_img = 255 - gray_img
+gray_img[gray_img > 100] = 255
+gray_img[gray_img <= 100] = 0
+# black_padding = np.zeros((50, width))
+# gray_img = np.row_stack((black_padding, gray_img))
+
+kernel = np.ones((30, 30), np.uint8)
+closing = cv2.morphologyEx(gray_img, cv2.MORPH_CLOSE, kernel)
+
+closingcopy = np.uint8(closing)
+
+edges = cv2.Canny(closingcopy, 100, 200)
+
+title = ['edges']
+images = [edges]
+
+for i in range(1):
+    plt.subplot(1,1,i+1),plt.imshow(images[i], 'gray')
+    plt.title(title[i])
+    plt.xticks([])
+    plt.yticks([])
+plt.show()
+
