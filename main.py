9e415da57d96562de73d12cfb474296ef67d8b35
krishna712001 <63452545+krishna712001@users.noreply.github.com>
2021-09-15 23:52:02 +0800

    Added comments and fixed variable names

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import matplotlib.pyplot as plt
 
 img = cv2.imread("Images/homer.jpeg")
 savingPath = "/Users/krishnakothandaraman/PycharmProjects/backgroundRemovalFromImage/processedImages"
-segmenter = SelfiSegmentation()
 
+# for turning background white for easier detection
+segmenter = SelfiSegmentation()
 imgOut = segmenter.removeBG(img, (255, 255, 255), threshold=0.5)
 
+# convert image to gray scale
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 height, width = gray_img.shape
 
@@ -25,19 +27,19 @@ gray_img[gray_img <= 100] = 0
 # gray_img = np.row_stack((black_padding, gray_img))
 
 kernel = np.ones((30, 30), np.uint8)
-closing = cv2.morphologyEx(gray_img, cv2.MORPH_CLOSE, kernel)
 
-closingcopy = np.uint8(closing)
+# fill inside of image
+filledImage = cv2.morphologyEx(gray_img, cv2.MORPH_CLOSE, kernel)
+filledImageNormalized = np.uint8(filledImage)
 
-edges = cv2.Canny(closingcopy, 100, 200)
+edges = cv2.Canny(filledImageNormalized, 100, 200)
 
 title = ['edges']
 images = [edges]
 
 for i in range(1):
-    plt.subplot(1,1,i+1),plt.imshow(images[i], 'gray')
+    plt.subplot(1, 1, i + 1), plt.imshow(images[i], 'gray')
     plt.title(title[i])
     plt.xticks([])
     plt.yticks([])
 plt.show()
-
